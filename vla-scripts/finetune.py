1cb1e0520f2b3fc2589f8926f272751d3b2e7001
jinchengli2022 <jincheng.li2022@gmail.com>
2026-03-01 01:56:46 +0800

    fix resume

--- vla-scripts/finetune.py
+++ vla-scripts/finetune.py
@@ -818,35 +818,37 @@ def finetune(cfg: FinetuneConfig) -> None:
                 hf_token=hf_token,
                 load_for_training=True,
                 )
-        config = AutoConfig.from_pretrained("pretrained_models/configs/config.json")
-        vla = AutoModelForVision2Seq.from_config(config, torch_dtype=torch.bfloat16).to(device_id)  # Create a new model with configuration, the parameters are randomly initialized
-        # for name, param in model.named_parameters():
-        #     print(f"{name}: {param.shape}")
-        replace_map = [
-            ("vision_backbone.dino_featurizer", "vision_backbone.featurizer"),
-            ("vision_backbone.siglip_featurizer", "vision_backbone.fused_featurizer"),
-            ("llm_backbone.llm", "language_model"),
-            ("projector.projector.0", "projector.fc1"),
-            ("projector.projector.2", "projector.fc2"),
-            ("projector.projector.4", "projector.fc3"),
-            ("gamma", "scale_factor"),
-            ]
 
-        def rename_state_dict_keys(state_dict, replace_map):
-            new_state_dict = {}
-            for k, v in state_dict.items():
-                new_k = k
-                for old, new in replace_map:
-                    if old in new_k:
-                        new_k = new_k.replace(old, new)
-                new_state_dict[new_k] = v
-            return new_state_dict
+        if not (cfg.resume and cfg.merge_lora_during_training):
+            config = AutoConfig.from_pretrained("pretrained_models/configs/config.json")
+            vla = AutoModelForVision2Seq.from_config(config, torch_dtype=torch.bfloat16).to(device_id)  # Create a new model with configuration, the parameters are randomly initialized
+            # for name, param in model.named_parameters():
+            #     print(f"{name}: {param.shape}")
+            replace_map = [
+                ("vision_backbone.dino_featurizer", "vision_backbone.featurizer"),
+                ("vision_backbone.siglip_featurizer", "vision_backbone.fused_featurizer"),
+                ("llm_backbone.llm", "language_model"),
+                ("projector.projector.0", "projector.fc1"),
+                ("projector.projector.2", "projector.fc2"),
+                ("projector.projector.4", "projector.fc3"),
+                ("gamma", "scale_factor"),
+                ]
+
+            def rename_state_dict_keys(state_dict, replace_map):
+                new_state_dict = {}
+                for k, v in state_dict.items():
+                    new_k = k
+                    for old, new in replace_map:
+                        if old in new_k:
+                            new_k = new_k.replace(old, new)
+                    new_state_dict[new_k] = v
+                return new_state_dict
+            
+            old_state_dict = vlm.state_dict()
+            RAW_STATE_DICT = rename_state_dict_keys(old_state_dict, replace_map)
         
-        old_state_dict = vlm.state_dict()
-        RAW_STATE_DICT = rename_state_dict_keys(old_state_dict, replace_map)
-    
-        missing_keys, unexpected_keys = vla.load_state_dict(RAW_STATE_DICT, strict=False)
-        del old_state_dict
+            missing_keys, unexpected_keys = vla.load_state_dict(RAW_STATE_DICT, strict=False)
+            del old_state_dict
 
     else:
         RAW_STATE_DICT ={}
